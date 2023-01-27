d29e0e41ceb03532d5cf924bd2edea3cd683f840
notnulldev <notnulldev0@gmail.com>
2023-01-27 10:42:45 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+	import Header from './header.svelte';
+	import Sidebar from './sidebar.svelte';
+</script>
+
+<div class="min-h-screen flex flex-col bg-gray-100">
+	<Header />
+	<div class="flex flex-1">
+		<Sidebar />
+		<div class="flex-1"><slot /></div>
+	</div>
+</div>
---
+++ src/routes/header.svelte
@@ -0,0 +1,3 @@
+<header class="p-4 flex justify-between">
+	<a class="p-2 px-4 hover:bg-slate-200 cursor-pointer rounded-xl" href="/">Management</a>
+</header>
